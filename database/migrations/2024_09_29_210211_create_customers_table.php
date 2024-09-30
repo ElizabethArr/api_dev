9b0a4c0e35bd821c416d7e47a19d97d596903af0
ElizabethArr <elizaarroyoe@gmail.com>
2024-09-30 15:23:59 -0600

    se modificaron campos

--- database/migrations/2024_09_29_210211_create_customers_table.php
+++ database/migrations/2024_09_29_210211_create_customers_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');  // Add name column
-            $table->string('last_name')->unique();  // Add email column and make it unique
+            $table->string('last_name');  // Add email column and make it unique
             $table->string('email')->unique();
-            $table->string('birth_date')->unique();
-            $table->string('personal_phone')->unique();
-            $table->string('contact_phone')->unique();
-            $table->string('password')->unique();
-            $table->string('zip_code')->unique();
+            $table->string('birth_date');
+            $table->string('personal_phone');
+            $table->string('contact_phone');
+            $table->string('password');
+            $table->string('zip_code');
+            $table->timestamps(); 
         });
     }
 
